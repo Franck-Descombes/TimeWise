b451aae073e353d385a2d339bb7d5a33d7c0a16d
Descombes Franck <franckdescombes@gmail.com>
2023-02-07 11:13:00 +0100

    add navbar component

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'al-navbar',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
+  homePath: string = 'home';
+  loginPath: string = 'login';
+  registerPath: string = 'register';
+
+  constructor(private router: Router) { } // Router injection
+
+  ngOnInit() { }
+
+  public isActive(page: string): boolean {
+    return this.router.isActive(page, { paths: 'exact', queryParams: 'exact', fragment: 'ignored', matrixParams: 'ignored' });  // Angular 12 ou >
+    return this.router.isActive(page, true);  // Angular 11 ou < 
+  }
+
+  public navigate(page: string): void {
+    this.router.navigate([page]);
+  }
 
 }
