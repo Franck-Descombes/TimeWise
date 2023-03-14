58368d6112638b3d5f708d6c8999ed566dc6e6a4
Descombes Franck <franckdescombes@gmail.com>
2023-03-14 17:07:41 +0100

    Intercept day change

--- src/app/protected/workday/workday-form-date/workday-form-date.component.ts
+++ src/app/protected/workday/workday-form-date/workday-form-date.component.ts
@@ -1,7 +1,9 @@
 // composant dédié à l’affichage et la gestion de la date de notre journée de travail, synchronisé avec le composant du formulaire principal.
-import { Component, OnInit, Input } from '@angular/core';
-import { FormControl } from '@angular/forms';
+// composant de gestion du champ date qui intercepte les dates sélectionnées par l’utilisateur, et les remontent au composant parent dans un format pertinent. 
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, Form } from '@angular/forms';
 import { BsLocaleService } from 'ngx-bootstrap/datepicker'; // service de langue
+import { DateService } from 'src/app/core/services/date.service';
 
 @Component({
   selector: 'al-workday-form-date',
@@ -11,12 +13,23 @@ import { BsLocaleService } from 'ngx-bootstrap/datepicker'; // service de langue
 })
 export class WorkdayFormDateComponent implements OnInit {
 
-  @Input() dueDate: FormControl; // déclare une propriété d’entrée. 
+  @Input() dueDate: FormControl;
+  @Output() dateSelected = new EventEmitter<string>();
 
-  constructor(private localeService: BsLocaleService) { } // injection du service
+  constructor(
+    private localeService: BsLocaleService,
+    private dateService: DateService) { }
 
   ngOnInit() {
-    this.localeService.use('fr'); // prend en paramètre une langue, qui doit préalablement avoir été chargé au niveau du module (ici, worday.module.ts)
+    this.localeService.use('fr'); // param de langue qui est chargé au niveau du workday.module.ts
   }
 
+  // intercèpte les dates via bsValueChange.
+  //Pour chaque date interceptée, on la transforme en sa date d’affichage grâce à la méthode getDisplayDate du service DateService.
+  selectDate(date: Date): void {
+    if (date) {
+      const displayDate: string = this.dateService.getDisplayDate(date);
+      this.dateSelected.emit(displayDate);
+    }
+  }
 }
