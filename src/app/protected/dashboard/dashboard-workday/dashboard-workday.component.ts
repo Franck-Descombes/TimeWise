48be5be60cffaeec4d3ab34104e2c91b26764927
Descombes Franck <franckdescombes@gmail.com>
2023-03-23 16:40:38 +0100

    add control buttons before PR

--- src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
+++ src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Workday } from 'src/app/shared/models/workday';
- 
+
 @Component({
   selector: 'al-dashboard-workday',
   templateUrl: './dashboard-workday.component.html',
   styleUrls: ['./dashboard-workday.component.scss']
 })
-export class DashboardWorkdayComponent {
+export class DashboardWorkdayComponent implements OnInit {
   @Input() workday: Workday;
+  isPomodoroActive: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.isPomodoroActive = false; // lorsque le user arrive sur le dashboard, le pomodoro n’est pas en train de s’exécuter.
+  }
+
+  // 3 méthodes qui permettrons respectivement de :
+  startPomodoro() {
+    this.isPomodoroActive = true;
+  }
+
+  cancelPomodoro() {
+    this.isPomodoroActive = false;
+  }
+
+  completePomodoro() {
+    this.isPomodoroActive = false;
+  }
 }
