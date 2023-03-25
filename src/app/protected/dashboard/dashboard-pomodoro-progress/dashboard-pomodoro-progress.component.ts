8a1ff290862e9ff8930bd4e27e6d341851a854b3
Descombes Franck <franckdescombes@gmail.com>
2023-03-25 09:56:07 +0100

    add dashboard pomodoro progress 1

--- src/app/protected/dashboard/dashboard-pomodoro-progress/dashboard-pomodoro-progress.component.ts
+++ src/app/protected/dashboard/dashboard-pomodoro-progress/dashboard-pomodoro-progress.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'al-dashboard-pomodoro-progress',
   templateUrl: './dashboard-pomodoro-progress.component.html',
   styleUrls: ['./dashboard-pomodoro-progress.component.scss']
 })
-export class DashboardPomodoroProgressComponent {
+export class DashboardPomodoroProgressComponent implements OnInit {
 
+  currentProgress: number;
+  percentage: number;
+  
+  // récupère les propriétés d’entrée transmis par le composant parent (DashboardWorkdayComponent)
+  @Input()
+  set progress(progress: number) {
+   this.currentProgress = progress;
+   this.computePercentage();
+  }
+  
+  @Input() maxProgress: number;
+
+  ngOnInit() {}
+
+  computePercentage() {
+    // check if emitted values are != 0
+    if (!this.currentProgress || !this.maxProgress) {
+      this.percentage = 0;
+      return;
+    }
+    // calcul du pourcentage de progression et MAJ de la loading bar
+    this.percentage = Math.floor(this.currentProgress / this.maxProgress * 100);
+  }
 }
--- src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
+++ src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { delay, interval, map, Observable, of, Subject, takeUntil, takeWhile } from 'rxjs';
+import { Task } from 'src/app/shared/models/task';
 import { Workday } from 'src/app/shared/models/workday';
 
 @Component({
@@ -9,23 +11,70 @@ import { Workday } from 'src/app/shared/models/workday';
 export class DashboardWorkdayComponent implements OnInit {
   @Input() workday: Workday;
   isPomodoroActive: boolean;
+  currentTask: Task | undefined;
+
+  // Initialisation des flux 
+  startPomodoro$: Subject<string>;
+  cancelPomodoro$: Subject<string>;
+  completePomodoro$: Subject<string>;
+  currentProgress: number;
+  maxProgress: number;
+  pomodoro$: Observable<number>;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.isPomodoroActive = false; // lorsque le user arrive sur le dashboard, le pomodoro n’est pas en train de s’exécuter.
+    this.isPomodoroActive = false;
+    this.startPomodoro$ = new Subject();
+    this.cancelPomodoro$ = new Subject();
+    this.completePomodoro$ = new Subject();
+    this.currentProgress = 0
+    this.maxProgress = 5; // valeur paramètrable par le user employee google.
+    this.pomodoro$ = interval(1000).pipe(// interval() crée un new flux qui émet les valeurs toutes les x millisec.
+      takeUntil(this.cancelPomodoro$), // désabonnements des flux : Opérateur takeUntil(), et sur les Subjects cancelPomodoro$ et completePomodoro$.
+      takeUntil(this.completePomodoro$),
+      takeWhile(progress => progress <= this.maxProgress), // se désabonne du flux dès que le pomodoro se termine.
+      map(x => x + 1));// map() transforme chaque valeur émise par un flux. On a bien la val 1 après 1 sec, puis 2 après 2 sec, etc.
   }
 
-  // 3 méthodes qui permettrons respectivement de :
+  // GPT SUGGESTION
   startPomodoro() {
     this.isPomodoroActive = true;
-  }
+    this.startPomodoro$.next('start');// emit a value in the subject (doesn't matter which one)
 
-  cancelPomodoro() {
-    this.isPomodoroActive = false;
+    this.pomodoro$.subscribe((currentProgress: number) => {// à chaque seconde écoulée depuis le début du pomodoro, on actualise la valeur de la propriété currentProgress.
+      this.currentProgress = currentProgress;
+
+      if (currentProgress === this.maxProgress) {
+        const completedPomodoros = currentProgress / 1500;
+        if (completedPomodoros === this.maxProgress) {
+          of(0).pipe(delay(500)).subscribe(_ => this.completePomodoro()); // possible erreur de compilation en raison du paramètre _ (implicitement de type any).
+
+
+        }
+      }
+    });
   }
 
   completePomodoro() {
     this.isPomodoroActive = false;
+    this.completePomodoro$.next('complete')
+
+    // step 1 : retrieve current task
+    this.currentTask = this.getCurrentTask();
+
+    // step 2 : increment current task
+    if (this.currentTask) {
+      this.currentTask.done++;
+    }
+  }
+
+  getCurrentTask(): Task | undefined {
+    return this.workday.tasks.find(task => task.todo > task.done)
+  }
+
+  cancelPomodoro() {
+    this.isPomodoroActive = false;
+    this.cancelPomodoro$.next('cancel')
   }
 }
